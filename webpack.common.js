eff4320faaab8171283680e49ca2f86ae2afd1f0
mghweb <maxghegler@gmail.com>
2019-07-19 13:29:37 -0700

    changed name

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
 	],
 	output: {
 		path: path.resolve( __dirname, 'dist' ),
-		filename: 'SSAPI.min.js',
+		filename: 'ssapi-wrapper.min.js',
 		library: 'SSAPI',
 		libraryTarget: 'umd',
 		globalObject: 'this'
