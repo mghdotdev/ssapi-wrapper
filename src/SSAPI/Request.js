410d745ce1522c90b7a71cb41cf3915ebf110fa8
Max Hegler <mhegler@miva.com>
2022-06-09 11:17:50 -0700

    import object hash; code formatting; link method param

--- src/SSAPI/Request.js
+++ src/SSAPI/Request.js
@@ -1,25 +1,25 @@
-import qs from 'qs'
+import hash from 'object-hash';
 import {RequestCache} from './RequestCache'
 
 const DONE = 4;
 const OK = 200;
 
-const cache = new RequestCache( 30000 );
+const cache = new RequestCache(30000);
 
 class Request {
 
-	constructor( endpoint, method, params ) {
+	constructor (endpoint, method, params) {
 		this.endpoint = endpoint;
 		this.method = method;
 		this.params = params;
 	}
 
-	serializeParams( params ) {
-		return qs.stringify( params, { arrayFormat: 'repeat' } )
+	serializeParams (params) {
+		return qs.stringify(params, { arrayFormat: 'repeat' })
 	}
 
-	buildUrl( endpoint, params ) {
-		if ( endpoint.indexOf( '?' ) > -1 ) {
+	buildUrl (endpoint, params) {
+		if (endpoint.indexOf('?') > -1) {
 			return `${ endpoint }${ params }`;
 		}
 		else {
@@ -27,61 +27,45 @@ class Request {
 		}
 	}
 
-	send() {
-
-		return new Promise(( resolve, reject ) => {
-
-			const queryString = this.serializeParams( { ...this.params } );
-			const url = this.buildUrl( this.endpoint, queryString );
-
-			const cacheResponse = cache.access( url );
-			if ( cacheResponse != undefined ) {
-				return resolve( cacheResponse );
+	send () {
+		return new Promise((resolve, reject) => {
+			const cacheResponse = cache.access(url);
+			if (cacheResponse != undefined) {
+				return resolve(cacheResponse);
 			}
 
 			const xhr = new XMLHttpRequest();
-			xhr.open( 'GET', url );
+			xhr.open(this.method, url);
 			xhr.responseType = 'json';
 			xhr.send();
 
 			xhr.onreadystatechange = () => {
-
-				if ( xhr.readyState == DONE ) {
-
-					if ( xhr.status == OK ) {
-
+				if (xhr.readyState == DONE) {
+					if (xhr.status == OK) {
 						const returnObject = {
 							response: {
 								status: xhr.status,
 								responseURL: xhr.responseURL,
 								responseType: xhr.responseType,
-								data: ( typeof xhr.response == 'object' ) ? xhr.response : JSON.parse( xhr.response )
+								data: (typeof xhr.response == 'object') ? xhr.response : JSON.parse(xhr.response)
 							},
 							requestParams: { ...this.params },
 							requestQueryString: queryString
 						};
 
-						cache.insert( url, returnObject );
+						cache.insert(url, returnObject);
 
-						resolve( returnObject );
-
+						resolve(returnObject);
 					}
 					else {
+						console.error(`[SSAPI][Request].send - Error with response.`, `Code: ${ xhr.status }.`);
 
-						console.error( `[SSAPI][Request].send - Error with response.`, `Code: ${ xhr.status }.` );
-
-						reject( xhr );
-
+						reject(xhr);
 					}
-
 				}
-
 			};
-
 		});
-
 	}
-
 };
 
 export {
